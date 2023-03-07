02a551109b1f9b291bb926a6bd931be4fa6fecaf
Maksim Kashin <sagiems@gmail.com>
2023-03-07 16:10:52 +0600

    токены

--- django_traders_diary/urls.py
+++ django_traders_diary/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshSlidingView, TokenVerifyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,4 +8,7 @@ urlpatterns = [
     path('api/v1/auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
     path('', include('core.urls')),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/', TokenRefreshSlidingView.as_view(), name='token_refresh'),
+    path('api/v1/token/', TokenVerifyView.as_view(), name='token_verity'),
 ]
